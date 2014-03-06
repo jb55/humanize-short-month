7134f5103d7c3d64c820d7f8249f9341bce18ddc
William Casarin <will@casarin.me>
2014-03-06 09:22:22 -0800

    1.0.0
    * Added tests for bug
    * Added changelog

---
+++ History.md
@@ -0,0 +1,8 @@
+
+# 1.0.0
+
+* Fixed issue where `jan` parsed to `December`
+
+# 0.0.0
+
+Initial commit
--- test/test.js
+++ test/test.js
@@ -3,8 +3,22 @@ var should = require('should');
 var humanize = require('..');
 
 describe('conversion', function(){
-  it('should work', function(){
+  it('with year should work', function(){
     humanize('dec2013').should.eql("December 2013")
+  });
+
+  it('months hould parse correctly', function(){
+    humanize('jan').should.eql("January")
+    humanize('feb').should.eql("February")
+    humanize('mar').should.eql("March")
+    humanize('apr').should.eql("April")
+    humanize('may').should.eql("May")
+    humanize('jun').should.eql("June")
+    humanize('jul').should.eql("July")
     humanize('aug').should.eql("August")
+    humanize('sep').should.eql("September")
+    humanize('oct').should.eql("October")
+    humanize('nov').should.eql("November")
+    humanize('dec').should.eql("December")
   });
 });
